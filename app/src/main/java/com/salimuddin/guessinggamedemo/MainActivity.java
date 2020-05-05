3442cda2ebd7d8cbc4629917cdb540984dbb0e38
SM tajuddin akash <smtajuddinakash@gmail.com>
2020-05-06 02:43:50 +0600

    I Make a Simple Guessing Game To learn about Random Number In Android Studio

---
+++ app/src/main/java/com/salimuddin/guessinggamedemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.salimuddin.guessinggamedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText ValueId;
+    private Button BtnId;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ValueId = findViewById(R.id.guessId);
+        BtnId = findViewById(R.id.btnId);
+        result = findViewById(R.id.result);
+
+        BtnId.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int GivenNumber = Integer.parseInt(ValueId.getText().toString());
+
+        //Random number Generate
+        Random random = new Random();
+        int randomNumber = random.nextInt(10)+1;
+
+        if (randomNumber == GivenNumber){
+            result.setText("Congratulation! You Won");
+        }
+        else {
+            result.setText("Sorry! You Lost. Generated Number is: "+randomNumber);
+        }
+
+
+    }
+}
